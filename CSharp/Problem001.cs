b04d6fcea46363d1ea0103b254a0fecb741407ff
IgorVenancio <41910277+IgorVenancio@users.noreply.github.com>
2024-02-11 19:15:56 -0300

    Problem 001, C#

---
+++ CSharp/Problem001.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectEuler
+{
+    internal class Problem001
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
